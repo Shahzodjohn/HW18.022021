c200c8c3a7c63b3c286329e74e6f14ff4fdf530b
Shahzod Akhmedov <77686662+Shahzodjohn@users.noreply.github.com>
2021-02-19 21:24:55 +0500

    Add files via upload

---
+++ Новая папка (3)/Асинхронное программирование.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task(Display);
+            task.Start();
+
+            Console.WriteLine("Завершение метода Main");
+
+            Console.ReadLine();
+        }
+
+        static void Display()
+        {
+            Console.WriteLine("Начало работы метода Display");
+
+            Console.WriteLine("Завершение работы метода Display");
+        }
+    }
+}
